35eb975d7378f4012c6b8bc830403655e2dd59c9
thescorpiolab <support@thescorpiolab.com>
2019-06-26 06:46:36 +0000

    Add Figaro Key & Secret

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "bb4b8a63f665d03b840e10969ed5ea5b"
-  config.secret = "8e230557c774a1265435026e6c69b4b3"
+  config.api_key = ENV['SHOPIFYAPIKEY']
+  config.secret = ENV['SHOPIFYAPISECRET']
   config.old_secret = "<old_secret>"
   config.scope = "read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
