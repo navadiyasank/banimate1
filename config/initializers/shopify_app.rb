44b1ae92cecd5df119b200486424f7f551f8b8b5
thescorpiolab <support@thescorpiolab.com>
2019-06-28 21:25:42 +0000

    Webhook Uninstalled App

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -3,7 +3,7 @@ ShopifyApp.configure do |config|
   config.api_key = ENV['SHOPIFY_API_KEY']
   config.secret = ENV['SHOPIFY_API_SECRET']
   config.old_secret = "<old_secret>"
-  config.scope = "read_products read_orders" # Consult this page for more scope options:
+  config.scope = "read_products" # Consult this page for more scope options:
                                  # https://help.shopify.com/en/api/getting-started/authentication/oauth/scopes
   config.embedded_app = true
   config.after_authenticate_job = false
@@ -14,16 +14,6 @@ ShopifyApp.configure do |config|
       topic: 'app/uninstalled',
       address: "https://797a170f.ngrok.io/webhooks/app_uninstalled",
       format: 'json'
-    },
-    {
-      topic: 'carts/create',
-      address: "https://797a170f.ngrok.io/webhooks/carts_create",
-      format: 'json'
-    },
-    {
-      topic: 'orders/paid',
-      address: "https://797a170f.ngrok.io/webhooks/orders_paid",
-      format: 'json'
-    },
+    }
   ]
 end
