7095ebae2f77e66a59358ec3513bd66cfbe0f71c
sanket navadiya <sanknavadiya162@gmail.com>
2019-07-08 22:11:00 +0530

    add snippets in main theme

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -2,6 +2,7 @@ class Shop < ActiveRecord::Base
   include ShopifyApp::SessionStorage
   has_one :popup_setting, dependent: :destroy
   after_create :set_configuration
+  after_create :asset_integrate
   after_update :set_configuration, if: ->(obj){ obj.saved_change_to_shopify_token? }
 
   def api_version
@@ -16,7 +17,15 @@ class Shop < ActiveRecord::Base
   end
 
   def asset_integrate
-  	
+  	ShopifyAPI::Base.site = "https://#{ShopifyApp.configuration.api_key}:#{self.shopify_token}@#{self.shopify_domain}/admin/"
+    @theme = ShopifyAPI::Theme.find(:all).where(role: 'main').first
+    @asset = ShopifyAPI::Asset.create(key: 'snippets/banimate.liquid', value: 'This is for testing ', theme_id: @theme.id) rescue nil
+
+    @asset = ShopifyAPI::Asset.find('layout/theme.liquid', :params => { :theme_id => @theme.id}) rescue nil
+    if @asset.present?
+      @asset_value = @asset.value
+      @asset.update_attributes(theme_id: @theme.id,value: @asset_value.gsub("</body>","{% comment %}This is for banimate.{% endcomment %}{% include 'banimate' %}</body>")) unless @asset_value.include?("{% include 'banimate' %}")
+    end
   end
 
 end
